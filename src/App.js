847ae78b839396a24e95dc83531aadef5b1e3400
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2022-12-24 23:26:06 +0530

    working with JSX dynamics syntax

--- src/App.js
+++ src/App.js
@@ -4,10 +4,30 @@ import Simplecard from './Components/Simplecard';
 import Navbar from './Components/Navbar';
 
 function App() {
+  const name = "Tharindu";
+  
+  // const myJsx = (
+    // <div>
+    {/* <h2 style={{color:'purple'}}>Hey</h2> */}
+     {/* </div> */}
+  // )
+  // function myalert() {
+  //   alert("Hello Tharindu");
+  // }
+  
+  const person = {
+    name:"Tharindu",
+    valu:"28",
+  }
+
   return (
     <>
-      <Simplecard />
-      <Navbar />
+      <h1 style={{ color: 'red', backgroundColor: 'white' }}>Hello {name} your
+        age is {8 * 3}</h1>
+      {/* {myalert()} */}
+      {/* { myJsx} */}
+
+      <h1 style={{ color: 'white' }}>Hey try again {person.name} { person.valu}</h1>
     </>
   );
 }
